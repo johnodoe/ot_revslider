b2774c99e15edbafef71dd1615ac0a0fc70b01f0
johnodoe <stefano.trabucco@gmail.com>
2015-05-26 16:45:17 +0200

    Create meta-boxes.php

---
+++ meta-boxes.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Initialize the custom Meta Boxes. 
+ */
+add_action( 'admin_init', 'custom_meta_boxes' );
+
+/**
+ * Meta Boxes demo code.
+ *
+ * You can find all the available option types in demo-theme-options.php.
+ *
+ * @return    void
+ * @since     2.0
+ */
+function custom_meta_boxes() {
+  
+  /**
+   * Create a custom meta boxes array that we pass to 
+   * the OptionTree Meta Box API Class.
+   */
+
+  $array_choices      = array();
+  if(shortcode_exists("rev_slider"))  { 
+    $new_slider       = new RevSlider();
+    $tot_revsliders   = $new_slider->getArrSliders();
+
+
+    foreach ( $tot_revsliders as $rev_single ) {
+         $alias   = $rev_single->getAlias();
+         $title   = $rev_single->getTitle();
+         array_push($array_choices, 
+                    array('value' => $alias,
+                          'label' =>$title,
+                          'src'   =>''));      
+    }
+    
+  /* 
+      PRINT THE REVOLUTION SLIDER IN THEMES 
+      
+      $my_slider        = get_post_meta($post->ID, 'slider_select', true); 
+      
+      if(function_exists("putRevSlider") { 
+        putRevSlider  ( $my_slider ); 
+      };
+
+    */
+  }
+
+  $my_meta_box = array(
+    'id'          => 'demo_meta_box',
+    'title'       => __( 'Demo Meta Box', 'theme-text-domain' ),
+    'desc'        => '',
+    'pages'       => array( 'post','page' ),
+    'context'     => 'normal',
+    'priority'    => 'high',
+    'fields'      => array(
+      array(
+        'label'       => __( 'Conditions', 'theme-text-domain' ),
+        'id'          => 'demo_conditions',
+        'type'        => 'tab'
+      ),
+      array(
+        'label'       => __( 'Show Gallery', 'theme-text-domain' ),
+        'id'          => 'demo_show_gallery',
+        'type'        => 'on-off',
+        'desc'        => sprintf( __( 'Shows the Gallery when set to %s.', 'theme-text-domain' ), '<code>on</code>' ),
+        'std'         => 'off'
+      ),
+      array(
+        'label'       => '',
+        'id'          => 'demo_textblock',
+        'type'        => 'textblock',
+        'desc'        => __( 'Congratulations, you created a gallery!', 'theme-text-domain' ),
+        'operator'    => 'and',
+        'condition'   => 'demo_show_gallery:is(on),demo_gallery:not()'
+      ),
+      array(
+        'label'       => __( 'Gallery', 'theme-text-domain' ),
+        'id'          => 'demo_gallery',
+        'type'        => 'gallery',
+        'desc'        => sprintf( __( 'This is a Gallery option type. It displays when %s.', 'theme-text-domain' ), '<code>demo_show_gallery:is(on)</code>' ),
+        'condition'   => 'demo_show_gallery:is(on)'
+      ),
+      array(
+        'label'       => __( 'More Options', 'theme-text-domain' ),
+        'id'          => 'demo_more_options',
+        'type'        => 'tab'
+      ),
+      array(
+        'label'       => __( 'Text', 'theme-text-domain' ),
+        'id'          => 'demo_text',
+        'type'        => 'text',
+        'desc'        => __( 'This is a demo Text field.', 'theme-text-domain' )
+      ),
+      array(
+        'label'       => __( 'Textarea', 'theme-text-domain' ),
+        'id'          => 'demo_textarea',
+        'type'        => 'textarea',
+        'desc'        => __( 'This is a demo Textarea field.', 'theme-text-domain' )
+      ),
+      array(
+        'id'          => 'slider_select',
+        'label'       => __( 'Revolution Slider', 'option-tree-theme' ),
+        'desc'        => __( 'Choose one of the revolution slider created.', 'option-tree-theme' ),
+        'type'        => 'select',
+        'choices'     => $array_choices
+      )
+    )
+  );
+ /**
+   * Register our meta boxes using the 
+   * ot_register_meta_box() function.
+   */
+  if ( function_exists( 'ot_register_meta_box' ) )
+    ot_register_meta_box( $my_meta_box );
+
+}
